132e46559d3cb255c9a4adffa7f2a7c509944c08
Bhanu Sri <your-email@gmail.com>
2026-04-16 16:09:26 +0530

    removed spacy and fixed parser

--- backend/resume_parser.py
+++ backend/resume_parser.py
@@ -1,21 +1,22 @@
 import pdfplumber
-import spacy
-
-nlp = spacy.load("en_core_web_sm")
 
 def extract_text(file):
     text = ""
     with pdfplumber.open(file) as pdf:
         for page in pdf.pages:
-            text += page.extract_text()
+            if page.extract_text():
+                text += page.extract_text()
     return text
+
 
 def extract_skills(text):
     skills_list = ["python", "java", "html", "css", "javascript", "sql", "react"]
+    
+    text = text.lower()
     found_skills = []
 
-    for word in text.lower().split():
-        if word in skills_list:
-            found_skills.append(word)
+    for skill in skills_list:
+        if skill in text:
+            found_skills.append(skill)
 
     return list(set(found_skills))
